975999ffeaa1ed644d204b18cc607f45031e40ac
AgileCodingLibrary <46870899+AgileCodingLibrary@users.noreply.github.com>
2022-09-23 19:03:13 +0100

    added signout feature.

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,28 +1,38 @@
 import { Link } from "react-router-dom";
+import React from "react";
+import { useLogout } from "../../hooks/useLogout";
 
 //styles and icons
 import "./Navbar.css";
 import Temple from "../../assets/temple.svg";
 
-import React from "react";
-
 export default function NavBar() {
+  const { logout, error, isPending } = useLogout();
   return (
-    <div className="navbar">
+    <div className='navbar'>
       <ul>
-        <li className="logo">
-          <img src={Temple} alt="logo" />
+        <li className='logo'>
+          <img src={Temple} alt='logo' />
           <span>The Dogo</span>
         </li>
 
         <li>
-          <Link to="/login">Login</Link>
+          <Link to='/login'>Login</Link>
         </li>
         <li>
-          <Link to="/signup">Signup</Link>
+          <Link to='/signup'>Signup</Link>
         </li>
         <li>
-          <button className="btn">LogOut</button>
+          {!isPending && (
+            <button className='btn' onClick={logout}>
+              LogOut
+            </button>
+          )}
+          {isPending && (
+            <button className='btn' disabled>
+              Logging out
+            </button>
+          )}
         </li>
       </ul>
     </div>
