744d8db03150d6c9fc7863b8d5c033232dc6d440
AgileCodingLibrary <46870899+AgileCodingLibrary@users.noreply.github.com>
2022-09-23 12:33:44 +0100

    updated firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,24 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 
-// Your web app's Firebase configuration
+// // Your web app's Firebase configuration - LAPTOP
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC_8lvejZUcNENa1lCBg4bioWgJbPdL8-A",
+//   authDomain: "tuesdaycom.firebaseapp.com",
+//   projectId: "tuesdaycom",
+//   storageBucket: "tuesdaycom.appspot.com",
+//   messagingSenderId: "623359261234",
+//   appId: "1:623359261234:web:56b23d541bd3b7af37da2b",
+// };
+
+// Your web app's Firebase configuration - DESKTOP
 const firebaseConfig = {
-  apiKey: "AIzaSyC_8lvejZUcNENa1lCBg4bioWgJbPdL8-A",
-  authDomain: "tuesdaycom.firebaseapp.com",
-  projectId: "tuesdaycom",
-  storageBucket: "tuesdaycom.appspot.com",
-  messagingSenderId: "623359261234",
-  appId: "1:623359261234:web:56b23d541bd3b7af37da2b",
+  apiKey: "AIzaSyCEKtjdf2ZXkx1YqjIAfnS07LogmnmH-uY",
+  authDomain: "tuesdaycom-b2f46.firebaseapp.com",
+  projectId: "tuesdaycom-b2f46",
+  storageBucket: "tuesdaycom-b2f46.appspot.com",
+  messagingSenderId: "922660978189",
+  appId: "1:922660978189:web:c903241af4fb3f410b5f44",
 };
 
 // Initialize Firebase
