2bbc2976f1cc8a0cd8aa35163398c687716f932d
AgileCodingLibrary <46870899+AgileCodingLibrary@users.noreply.github.com>
2022-09-24 10:59:46 +0100

    Added user Avatar component and redirects.

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import React from "react";
 import { useLogout } from "../../hooks/useLogout";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 //styles and icons
 import "./Navbar.css";
 import Temple from "../../assets/temple.svg";
 
 export default function NavBar() {
-  const { logout, error, isPending } = useLogout();
+  const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
   return (
     <div className='navbar'>
       <ul>
@@ -15,25 +17,32 @@ export default function NavBar() {
           <img src={Temple} alt='logo' />
           <span>The Dogo</span>
         </li>
-
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-        <li>
-          <Link to='/signup'>Signup</Link>
-        </li>
-        <li>
-          {!isPending && (
-            <button className='btn' onClick={logout}>
-              LogOut
-            </button>
-          )}
-          {isPending && (
-            <button className='btn' disabled>
-              Logging out
-            </button>
-          )}
-        </li>
+        {!user && (
+          <li>
+            <Link to='/login'>Login</Link>
+          </li>
+        )}
+        {!user && (
+          <li>
+            <Link to='/signup'>Signup</Link>
+          </li>
+        )}
+        {user && (
+          <div>
+            <li>
+              {!isPending && (
+                <button className='btn' onClick={logout}>
+                  LogOut
+                </button>
+              )}
+              {isPending && (
+                <button className='btn' disabled>
+                  Logging out
+                </button>
+              )}
+            </li>
+          </div>
+        )}
       </ul>
     </div>
   );
